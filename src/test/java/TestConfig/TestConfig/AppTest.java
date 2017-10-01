f7226a99c04bcd495a81c4ec7613ccc63ba9c07c
DeNy <DeNy@IamDeNy>
2017-10-01 12:53:22 +0530

    dvc

--- src/test/java/TestConfig/TestConfig/AppTest.java
+++ src/test/java/TestConfig/TestConfig/AppTest.java
@@ -1,28 +1,16 @@
 package TestConfig.TestConfig;
 
 import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-    }
 
-    /**
-     * Rigourous Test :-)
-     */
-    @Test
+	@Test
 	public void testApp()
-    {
-        AssertJUnit.assertTrue( true );
-    }
+	{
+		System.out.println("Hello");
+	}
 }
