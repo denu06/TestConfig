b18197ca0b2b6d9b683db39a7b05cadbcde32e36
DeNy <DeNy@IamDeNy>
2017-10-01 16:07:32 +0530

    drgfvhbjnk

--- src/main/java/TestConfig/TestConfig/Testing.java
+++ src/main/java/TestConfig/TestConfig/Testing.java
@@ -6,6 +6,8 @@ public class Testing
 	public static void main(String string)
 	{
 		System.out.println("Testing project");
+		String user = System.getProperty("PerfectoUsername");
+		System.out.println("Username :" + user);
 	}
 
 }
